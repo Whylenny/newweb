419d8ba120f882adb186320a8255be98e864e1d4
Whylenny <107232315+Whylenny@users.noreply.github.com>
2022-12-31 16:06:17 -0800

    dec 31

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-var currently = ['index3'];
-
-function goto(where){
-  console.log(where);
-  const newLoc = where + '.html';
- //Scurrently.push(where);
- window.location.href = newLoc;
- 
-}
-
-function show(pic){
-    console.log(pic);
- const newLoc = pic + '.html'
- window.location.href = newLoc;
-}
-
 console.log("start");
-document.getElementByID("nutrition").onmouseover = function() {
-  document.getElementById("1nutrition").style.display = "block";
-}
-document.getElementByID("nutrition").onmouseout = function() {
-  document.getElementById("1nutrition").style.display = "none";
-}
+const mouseOver = (hotspot) => {
+  document.getElementById(hotspot).style.display = "block";
+  console.log("block");
+};
+
+const mouseOut = (hotspot) => {
+  document.getElementById(hotspot).style.display = "none";
+  console.log("none");
+};
+console.log("started");
